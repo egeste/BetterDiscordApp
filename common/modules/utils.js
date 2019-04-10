66c13e1f2cb900b9b9ee59049c1446cd55bc2d3e
Zack Rauen <rauenzi@outlook.com>
2019-04-10 11:15:04 -0400

    fix formatString

--- common/modules/utils.js
+++ common/modules/utils.js
@@ -66,7 +66,7 @@ export class Utils {
         for (const val in values) {
             let replacement = values[val];
             if (Array.isArray(replacement)) replacement = JSON.stringify(replacement);
-            if (typeof(replacement) === 'object' && replacement !== null) replacement = JSON.stringify(replacement);
+            if (typeof(replacement) === 'object' && replacement !== null) replacement = replacement.toString();
             string = string.replace(new RegExp(`{{${val}}}`, 'g'), replacement);
         }
         return string;
